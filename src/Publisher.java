bb4b7b6c997db5c498070fc6244e2e46ef952a48
PushkarrajPujari <pushkarraj@pujari.me>
2017-09-04 01:39:30 +0530

    Sanity Check

---
+++ src/Publisher.java
@@ -0,0 +1,12 @@
+/**
+ * @Author : Pushkarraj Pujari
+ * @Since  : 03/09/2017
+ */
+public class Publisher {
+    /*
+    * Sanity check
+    * */
+    public static void main(String[] args) {
+        System.out.println("Publisher Subscriber");
+    }
+}
